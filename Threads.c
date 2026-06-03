5830ae566a65ed72dda2ec10fe76f443e5d7d91d
nithishmanikandan-git <nithishmanikandanl7@gmail.com>
2026-06-03 13:03:26 +0530

    Update Threads.c

--- Threads.c
+++ Threads.c
@@ -6,9 +6,7 @@
 
 int N;
 
-/*
-Write a C program to define 3 different threads with the following purposes where N is the input . Thread A - To run a loop and return the sum of first N prime numbers . Thread B & C - should run in parallel. One prints "Thread 1 running" every 2 seconds, and the other prints "Thread 2 running" every 3 seconds for 100 seconds. 2. In the above program, . add signal handling for SIGINT (etc) and prevent termination. . Convert the above threads to individual functions and note down the time taken and the flow of execution.
-*/
+
 void handler(int signal){
 	if(signal == SIGINT) printf("\nSIGINT received!.\n Termination is prevented.\n");
 }
